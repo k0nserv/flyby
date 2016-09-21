8531130a9caca1e29c634407d51adb8347a2e871
Charlie Gildawie <charlie@skyscanner.net>
2016-09-21 15:55:51 +0100

    Whitespace service fix (#22)
    * whitespace fix
    
    * more jinja fixes

--- setup_test_foo.sh
+++ setup_test_foo.sh
@@ -6,3 +6,6 @@ curl -X POST -H "Content-Type: application/json" -d '{"resolver_name": "dnsmasq"
 curl -X POST -H "Content-Type: application/json" -d '{"name": "foo", "fqdn": "foo.example.com", "failover_pool_fqdn": "bar:80", "healthcheck_interval": 1000, "dns_resolver": "dnsmasq"}' "http://localhost:5000/service"
 curl -X POST -H "Content-Type: application/json" -d '{"service_name": "foo", "target_group_name": "backend", "weight": 50}' "http://localhost:5000/target"
 curl -X POST -H "Content-Type: application/json" -d '{"host": "foo:80", "service_name": "foo", "target_group_name": "backend"}' "http://localhost:5000/backend"
+
+# 2nd service
+curl -X POST -H "Content-Type: application/json" -d '{"name": "foo2", "fqdn": "foo2.example.com", "failover_pool_fqdn": "bar:80", "healthcheck_interval": 1000, "dns_resolver": "dnsmasq"}' "http://localhost:5000/service"
