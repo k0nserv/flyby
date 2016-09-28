382cca50f3649771567121315d84400a787764a4
steviehailey-skyscanner <stephen.hailey@skyscanner.net>
2016-09-28 16:17:18 +0100

    Ssl failover improvements (#29)
    * Correction to readme
    
    * Removing static test cert
    Now generate the cert as required when the test SSL container is created
    Switched to alpine variant of latest haproxy image for test SSL image
    
    * Adding support for SSL backend with valid signed certificates
    Current version bypasses the SSL cert but now we check against the appropriate CA
    Required installing the up to date CA list on the image and configuring haproxy to see the merged CA file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.4-alpine
 RUN apk add --update haproxy
+RUN apk add --update ca-certificates
+RUN update-ca-certificates
 WORKDIR /usr/src/app
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
--- setup_test_foo_bar.sh
+++ setup_test_foo_bar.sh
@@ -3,7 +3,7 @@
 # resolver Registration
 curl -X POST -H "Content-Type: application/json" -d '{"resolver_name": "dnsmasq", "nameserver_address": "dnsmasq"}' "http://localhost:5000/resolver"
 
-# foo.example.com with default to 'foo' and failover to HTTPS backup 'bar' (self-signed cert)
+# foo.example.com with default to 'foo' and failover to HTTPS backup 'bar'
 curl -X POST -H "Content-Type: application/json" -d '{"name": "foo", "fqdn": "foo.example.com", "failover_pool_fqdn": "bar:80", "healthcheck_interval": 1000, "dns_resolver": "dnsmasq"}' "http://localhost:5000/service"
 curl -X POST -H "Content-Type: application/json" -d '{"service_name": "foo", "target_group_name": "backend", "weight": 50}' "http://localhost:5000/target"
 curl -X POST -H "Content-Type: application/json" -d '{"host": "foo:80", "service_name": "foo", "target_group_name": "backend"}' "http://localhost:5000/backend"
