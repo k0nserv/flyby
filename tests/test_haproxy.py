7db141db0cd4ff15d4b1918fef7e94e116bc5e40
steviehailey-skyscanner <stephen.hailey@skyscanner.net>
2016-09-29 08:35:21 +0100

    Adding test coverage for recent changes to support SSL backend failover (#30)
    Includes tests for basic failover, resolvers and the new SSL options

--- tests/test_haproxy.py
+++ tests/test_haproxy.py
@@ -43,3 +43,116 @@ def test_harproxy_update_config(tmpdir, mocker):
     assert "server foo-foo-blue1 10.0.0.1:80 check inter 10s weight 80" in updated_config
     assert "server foo-foo-green2 10.0.0.2:81 check inter 10s weight 20" in updated_config
     assert "server foo-foo-green3 10.0.0.3:81 check inter 10s weight 20" in updated_config
+
+
+def test_harproxy_update_config_supports_resolver(tmpdir, mocker):
+    mocker.patch('flyby.haproxy.subprocess')
+    config = tmpdir.join("haproxy.cfg")
+    config.write("ha config")
+    ha = Haproxy(config_path=str(config))
+    resolvers = [
+        {
+            'name': 'my-resolver',
+            'nameserver_address': 'dnsmasq',
+            'nameserver_port': '9053',
+            'resolve_retries': '7',
+            'timeout_retry': '10',
+            'hold_valid': '42'
+        }
+    ]
+    services = [
+        {
+            'name': 'foo',
+            'fqdn': 'foo.example.com',
+            'healthcheck_path': '/',
+            'healthcheck_interval': 10,
+            'healthcheck_rise': 10,
+            'healthcheck_fall': 3,
+            'dns_resolver': 'my-resolver',
+            'target_groups': [
+                {
+                    "target_group_name": "foo-blue",
+                    "weight": 100,
+                    "backends": [
+                        {'host': '10.0.0.1:80'}
+                    ]
+                }
+            ]
+        }
+    ]
+    ha.update(services=services, resolvers=resolvers)
+    updated_config = config.read()
+    assert "resolvers my-resolver" in updated_config
+    assert "nameserver            nameserver dnsmasq:9053" in updated_config
+    assert "resolve_retries       7" in updated_config
+    assert "timeout retry         10" in updated_config
+    assert "hold valid            42" in updated_config
+    assert "server foo-foo-blue1 10.0.0.1:80 check inter 10s weight 100  resolvers my-resolver" in updated_config
+
+
+def test_harproxy_update_config_supports_ssl_backend_failover(tmpdir, mocker):
+    mocker.patch('flyby.haproxy.subprocess')
+    config = tmpdir.join("haproxy.cfg")
+    config.write("ha config")
+    ha = Haproxy(config_path=str(config))
+    services = [
+        {
+            'name': 'foo',
+            'fqdn': 'foo.example.com',
+            'failover_pool_fqdn': 'bar.example.com',
+            'failover_pool_use_https': 1,
+            'healthcheck_path': '/',
+            'healthcheck_interval': 10,
+            'healthcheck_rise': 10,
+            'healthcheck_fall': 3,
+            'target_groups': [
+                {
+                    "target_group_name": "foo-blue",
+                    "weight": 100,
+                    "backends": [
+                        {'host': '10.0.0.1:80'}
+                    ]
+                }
+            ]
+        }
+    ]
+    ha.update(services=services)
+    updated_config = config.read()
+    assert "ca-base      /etc/ssl/certs" in updated_config
+    assert "option httpchk GET / HTTP/1.1\\r\\nHost:\ foo.example.com" in updated_config
+    assert "http-request set-header X-Forwarded-Proto https" in updated_config
+    assert "server foo-foo-blue-backup bar.example.com backup check inter 10s  resolvers    ssl ca-file " \
+           "ca-certificates.crt" in updated_config
+
+
+def test_harproxy_update_config_supports_ssl_backend_failover_with_self_signed_cert(tmpdir, mocker):
+    mocker.patch('flyby.haproxy.subprocess')
+    config = tmpdir.join("haproxy.cfg")
+    config.write("ha config")
+    ha = Haproxy(config_path=str(config))
+    services = [
+        {
+            'name': 'foo',
+            'fqdn': 'foo.example.com',
+            'failover_pool_fqdn': 'bar.example.com',
+            'failover_pool_use_https': 1,
+            'failover_pool_ssl_verify_none': 1,
+            'healthcheck_path': '/',
+            'healthcheck_interval': 10,
+            'healthcheck_rise': 10,
+            'healthcheck_fall': 3,
+            'target_groups': [
+                {
+                    "target_group_name": "foo-blue",
+                    "weight": 100,
+                    "backends": [
+                        {'host': '10.0.0.1:80'}
+                    ]
+                }
+            ]
+        }
+    ]
+    ha.update(services=services)
+    updated_config = config.read()
+    assert "server foo-foo-blue-backup bar.example.com backup check inter 10s  resolvers    ssl ca-file " \
+           "ca-certificates.crt   verify none" in updated_config
