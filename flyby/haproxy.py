bb7bb994c68221ec4cc81ce1dcc2ceb4181b0310
Balint Zsilavecz <balint.zsilavecz@skyscanner.net>
2016-10-18 12:25:39 +0100

    add debug logs to scheduled job

--- flyby/haproxy.py
+++ flyby/haproxy.py
@@ -5,7 +5,7 @@ import time
 from jinja2 import Environment, PackageLoader
 
 
-logger = logging.getLogger("haproxy")
+logger = logging.getLogger(__name__)
 metrics = logging.getLogger("metrics")
 
 
@@ -16,6 +16,7 @@ class Haproxy:
     def __init__(self, config_path="/etc/haproxy/haproxy.cfg"):
         self.config_path = config_path
         self.command = ['/usr/sbin/haproxy', '-f', config_path, '-db', '-q']
+        logger.debug("Haproxy initialised: {}".format(str(self.command)))
 
     def _filter_services(self, services):
         """
@@ -63,47 +64,71 @@ class Haproxy:
         return filtered_services
 
     def update(self, services, fqdn="flyby.example.com", resolvers=None):
+        logger.debug("Updating HAProxy configs...")
         resolvers = resolvers if resolvers else []
         env = Environment(loader=PackageLoader('flyby', 'config'))
         template = env.get_template('haproxy.cfg.j2')
         c = template.render(fqdn=fqdn, services=self._filter_services(services), resolvers=resolvers)
         if self.config != c:
+            logger.debug("Changed configs identified.")
             self.config = c
             self._run()
+            logger.debug("HAProxy configs successfully updated.")
+        else:
+            logger.debug("HAProxy configs up-to-date. Nothing to do.")
 
     @property
     def config(self):
+        logger.debug("Opening config file for reading: {}".format(self.config_path))
         with open(self.config_path, 'r') as f:
-            return f.read()
+            logger.debug("Reading config from: {}".format(self.config_path))
+            config = f.read()
+            logger.debug("Config successfully read from file.")
+            return config
 
     @config.setter
     def config(self, value):
+        logger.debug("Opening config file for writing: {}".format(self.config_path))
         with open(self.config_path, 'w') as f:
-            return f.write(value)
+            logger.debug("Writing config to: {}".format(self.config_path))
+            f.write(value)
+            logger.debug("Config successfully written to file.")
 
     def _run(self):
         def _wait_pid(p):
             if p:
+                logger.debug("Terminating old HAProxy process...")
                 start_time = time.time()
                 pid = p.pid
+                logger.debug("Waiting for HAProxy(PID:{}) to shut down...".format(str(pid)))
                 p.wait()
                 metrics.info('haproxy-shutdown.duration {}'.format(time.time() - start_time))
                 logger.info("HAProxy(PID:{}) has been terminated".format(str(pid)))
+            else:
+                logger.debug("Old HAProxy process not found.")
 
         if Haproxy.process:
             # Reload haproxy
-            logger.info("Reloading HAProxy")
-            process = subprocess.Popen(
-                self.command + ["-sf", str(Haproxy.process.pid)]
-            )
+            logger.info("Reloading HAProxy...")
+            command = self.command + ["-sf", str(Haproxy.process.pid)]
+            process = subprocess.Popen(command)
             old_process = Haproxy.process
-            Thread(target=_wait_pid, args=(old_process,)).start()
+            logger.debug("Starting shutdown thread...")
+            shutdown_thread = Thread(target=_wait_pid, args=(old_process,))
+            shutdown_thread.start()
+            logger.debug("Shutdown thread started ({}).".format(shutdown_thread.name))
             Haproxy.process = process
             logger.info(
-                "HAProxy has been reloaded(PID: {})".format(str(Haproxy.process.pid)))
+                "HAProxy has been reloaded (PID: {}): {}".format(
+                    str(Haproxy.process.pid), " ".join(command)
+                )
+            )
         else:
             # Launch haproxy
-            logger.info("Launching HAProxy")
+            logger.info("Launching HAProxy...")
             Haproxy.process = subprocess.Popen(self.command)
             logger.info(
-                "HAProxy has been launched(PID: {})".format(str(Haproxy.process.pid)))
+                "HAProxy has been launched (PID: {}): {}".format(
+                    str(Haproxy.process.pid), " ".join(self.command)
+                )
+            )
